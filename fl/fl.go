390aba4c912340b2e66b6bd86ac4eae5c6448640
wirekang <wirekang@gmail.com>
2020-11-02 19:50:16 +0900

    Add MakeIfNotExists

--- fl/fl.go
+++ fl/fl.go
@@ -7,3 +7,11 @@ func IsExists(path string) bool {
 	_, err := os.Stat(path)
 	return !os.IsNotExist(err)
 }
+
+// MakeIfNotExists makes directory if it doesn't exist.
+func MakeIfNotExists(path string) error {
+	if !IsExists(path) {
+		return os.MkdirAll(path, 0755)
+	}
+	return nil
+}
