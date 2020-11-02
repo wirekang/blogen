e12adc31d521be605b6fd974eb6f9074debcaff2
wirekang <wirekang@gmail.com>
2020-11-02 19:51:02 +0900

    Fix typo

--- fl/fl.go
+++ fl/fl.go
@@ -2,7 +2,7 @@ package fl
 
 import "os"
 
-// IsExists returns true when file is exists.
+// IsExists returns true if file exist.
 func IsExists(path string) bool {
 	_, err := os.Stat(path)
 	return !os.IsNotExist(err)
