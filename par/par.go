3fac4b8e4ed754591aca30b6dbe5b18fc4abd462
wirekang <wirekang@gmail.com>
2020-11-03 23:46:56 +0900

    Add Extract, DateToTime, MarkdownToHTML

---
+++ par/par.go
@@ -0,0 +1,35 @@
+package par
+
+import (
+	"errors"
+	"time"
+
+	"github.com/gomarkdown/markdown"
+)
+
+// Extract extracts settings and markdown bytes from src.
+// Settings and markdown are separated by sep.
+func Extract(src []byte, sep byte) (settings []byte, markdown []byte, err error) {
+	for i, b := range src {
+		if b == sep {
+			settings = src[:i]
+			markdown = src[i+1:]
+			return
+		}
+	}
+	return nil, nil, errors.New("can't sep by " + string(sep))
+}
+
+// MarkdownToHTML converts markdown bytes to html bytes.
+func MarkdownToHTML(md []byte) (html []byte) {
+	return markdown.ToHTML(md, nil, nil)
+}
+
+// DateToTime parses date from following format:
+//
+// - - -
+//
+// 2020-12-30
+func DateToTime(date string) (time.Time, error) {
+	return time.Parse("2006-1-2", date)
+}
---
+++ par/par_test.go
@@ -0,0 +1,34 @@
+package par
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/wirekang/blogen/er"
+)
+
+func TestExtract(t *testing.T) {
+	str := []byte("key1: val1\nkey2: val2\n$\n# this is markdown")
+	set, md, err := Extract(str, '#')
+	if er.PrintIfNotNil(err) {
+		t.FailNow()
+	}
+	fmt.Printf("==%s==\n==%s==\n", set, md)
+}
+
+func TestDateToTime(t *testing.T) {
+	s := "2020-8-12"
+	ti, e := DateToTime(s)
+	if e != nil {
+		t.FailNow()
+	}
+	if ti.Day() != 12 {
+		t.FailNow()
+	}
+	if ti.Month() != 8 {
+		t.FailNow()
+	}
+	if ti.Year() != 2020 {
+		t.FailNow()
+	}
+}
