7643f4dec8c2a92a08f7cd91591f984cd5001a90
wirekang <wirekang@gmail.com>
2020-11-05 19:39:23 +0900

    Update package fl

--- fl/fl.go
+++ fl/fl.go
@@ -1,28 +1,167 @@
-// Package fl serves uitl functions about files.
+//Package fl handle files.
 package fl
 
 import (
-	"os"
+	"path"
+
+	"github.com/wirekang/fileutil"
 )
 
-// IsExist returns true if file exist.
-func IsExist(path string) bool {
-	_, err := os.Stat(path)
-	return !os.IsNotExist(err)
+type directory struct {
+	path        string
+	isNecessary bool
 }
 
-// MakeIfNotExist makes directory if it doesn't exist.
-func MakeIfNotExist(path string) error {
-	if !IsExist(path) {
-		return os.MkdirAll(path, 0755)
+type file struct {
+	path    string
+	example string
+}
+
+var dirs = make([]directory, 0)
+var files = make([]file, 0)
+
+var root = directory{"", false}
+var configFile = newFile(root, "blogen.config",
+	"title: Title of blog\naddr: //example.com")
+
+var mdDir = newDirectory(root, "md", true)
+
+var templateDir = newDirectory(root, "template", true)
+var baseFile = newFile(templateDir, "base.html",
+	"<html></html>")
+var mainFile = newFile(templateDir, "main.html", "<div></div>")
+var listFile = newFile(templateDir, "list.html", "<div></div>")
+var singleFile = newFile(templateDir, "single.html", "<div></div>")
+var styleFile = newFile(templateDir, "style.css", "h1 {\n\n}")
+
+var outDir = newDirectory(root, "out", false)
+
+var genDir = newDirectory(root, "gen", false)
+var hashDir = newDirectory(genDir, "hash", false)
+var htmlDir = newDirectory(genDir, "html", false)
+
+func newDirectory(parent directory, name string, isNecessary bool) directory {
+	d := directory{
+		path:        path.Join(parent.path, name),
+		isNecessary: isNecessary,
+	}
+	dirs = append(dirs, d)
+	return d
+}
+
+func newFile(parent directory, name string, example string) file {
+	f := file{
+		path:    path.Join(parent.path, name),
+		example: example,
+	}
+	files = append(files, f)
+	return f
+}
+
+// IsNecessaryDirsExist returns true if all necessary directories exist.
+func IsNecessaryDirsExist() bool {
+	for _, dir := range dirs {
+		if !dir.isNecessary {
+			continue
+		}
+		if !fileutil.IsExist(dir.path) {
+			return false
+		}
+	}
+	return true
+}
+
+// MakeNecessaryDirs makes all necessary directories.
+func MakeNecessaryDirs() error {
+	for _, dir := range dirs {
+		if !dir.isNecessary {
+			continue
+		}
+		err := fileutil.MakeIfNotExist(dir.path)
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
 
-// CreateIfNotExist creates file if it doesn't exist.
-func CreateIfNotExist(name string) (*os.File, error) {
-	if !IsExist(name) {
-		return os.Create(name)
+// IsFilesExist returns true if all files exist.
+func IsFilesExist() bool {
+	for _, file := range files {
+		if !fileutil.IsExist(file.path) {
+			return false
+		}
 	}
-	return nil, nil
+	return true
+}
+
+// CreateExampleFiles creates all files with example content.
+func CreateExampleFiles() error {
+	for _, file := range files {
+		f, err := fileutil.CreateIfNotExist(file.path)
+		if err != nil {
+			return err
+		}
+		_, err = f.WriteString(file.example)
+		if err != nil {
+			return err
+		}
+		f.Close()
+	}
+	return nil
+}
+
+// Config returns file path of config file.
+func Config() string {
+	return configFile.path
+}
+
+// Base returns file path of base.html.
+func Base() string {
+	return baseFile.path
+}
+
+// Main returns file path of main.html.
+func Main() string {
+	return mainFile.path
+}
+
+// List returns file path of list.html.
+func List() string {
+	return listFile.path
+}
+
+// Single returns file path of single.html.
+func Single() string {
+	return singleFile.path
+}
+
+// Style returns file path of style.html.
+func Style() string {
+	return styleFile.path
+}
+
+// MarkdownDir returns path of markdown directory.
+func MarkdownDir() string {
+	return mdDir.path
+}
+
+// TemplateDir returns path of template directory.
+func TemplateDir() string {
+	return templateDir.path
+}
+
+// HashDir returns path of hash generated from markdown directory.
+func HashDir() string {
+	return hashDir.path
+}
+
+// HTMLDir returns path of html parsed from markdown directory.
+func HTMLDir() string {
+	return htmlDir.path
+}
+
+// OutDir returns path of output directory.
+func OutDir() string {
+	return outDir.path
 }
---
+++ fl/fl_test.go
@@ -0,0 +1,44 @@
+package fl
+
+import (
+	"os"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	os.Mkdir("tmp", 0755)
+	os.Chdir("tmp")
+	if IsNecessaryDirsExist() {
+		t.FailNow()
+	}
+	if IsFilesExist() {
+		t.FailNow()
+	}
+	err := MakeNecessaryDirs()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = CreateExampleFiles()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !IsNecessaryDirsExist() {
+		t.FailNow()
+	}
+	if !IsFilesExist() {
+		t.FailNow()
+	}
+	t.Log(Config())
+	t.Log(Base())
+	t.Log(Main())
+	t.Log(List())
+	t.Log(Single())
+	t.Log(Style())
+	t.Log(MarkdownDir())
+	t.Log(HTMLDir())
+	t.Log(TemplateDir())
+	t.Log(HashDir())
+	t.Log(OutDir())
+	os.Chdir("..")
+	os.RemoveAll("tmp")
+}
