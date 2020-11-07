7e796885b39196abd6fa32b153c29d5c707d3669
wirekang <wirekang@gmail.com>
2020-11-07 12:10:47 +0900

    Add -src=path option

--- main.go
+++ main.go
@@ -1,5 +1,43 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/wirekang/blogen/fl"
+)
+
 func main() {
+	init := flag.Bool("i", false, "initialization")
+	src := flag.String("src", "", "source directory")
+
+	flag.Parse()
+
+	if *src != "" {
+		err := os.Chdir(*src)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+	if *init {
+		err := fl.MakeDirs()
+		if err != nil {
+			fmt.Printf("dir: %s", err)
+			os.Exit(1)
+		}
+		err = fl.CreateExampleFiles()
+		if err != nil {
+			fmt.Printf("files: %s", err)
+			os.Exit(1)
+		}
+		fmt.Println("Done")
+		os.Exit(0)
+	}
+	if !fl.IsNecessaryDirsExist() || !fl.IsFilesExist() {
+		fmt.Println("Can't find necessary files. Add -i option for initialization.")
+		os.Exit(1)
+	}
 
 }
